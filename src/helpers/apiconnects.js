79a1f018dabdedfb76354f51f1397c4fc2705917
khasim sharif shaik <khasimsharif30@gmail.com>
2021-06-27 17:10:15 +0530

    add backend connection

--- src/helpers/apiconnects.js
+++ src/helpers/apiconnects.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://localhost:8000",
+    // baseURL: "http://localhost:8000",
+    baseURL: "https://invoice-backend-myself-beaware.herokuapp.com/",
     timeout: 10000,
     headers: {"Content-Type":"application/json"},
 })
